e616efbfaaae8202a658901e19605e2cd4299f24
Leppy-oss <70586381+Leppy-oss@users.noreply.github.com>
2024-07-14 21:58:56 -0500

    get started on graphQL and events API

--- pages/api/DocumentRetriever.js
+++ pages/api/DocumentRetriever.js
@@ -2,9 +2,9 @@ import { BaseRetriever } from '@langchain/core/retrievers';
 
 // Very basic retriever that returns all documents available
 export class DocumentRetriever extends BaseRetriever {
-    lc_namespace = ['langchain', 'retrievers'];
+	lc_namespace = ['langchain', 'retrievers'];
 
-    constructor(docs) { super({}); this.docs = docs; }
+	constructor(docs) { super({}); this.docs = docs; }
 
-    async _getRelevantDocuments(_, __) { return [...this.docs]; }
+	async _getRelevantDocuments(_, __) { return this.docs; }
 }
--- pages/api/storage.js
+++ pages/api/storage.js
@@ -1,22 +1,33 @@
 const teams = {};
 
 const storageContains = (season, eventCode) => Object.hasOwn(teams, season) && Object.hasOwn(teams[season], eventCode);
-const populateStorage = async (season, eventCode) => {
-    const fTeams = await (await fetch(`${process.env.EVENTS_URL}/${season}/${eventCode}/teams`)).json();
-    teams[season] = {};
-    teams[season][eventCode] = {};
-    fTeams.forEach(team => teams[season][eventCode][team.teamNumber] = {
-        teamNumber: team.teamNumber,
-        stats: team.stats
-    });
+const populateStorage = async(season, eventCode) => {
+	const fTeams = await (await fetch(`${process.env.EVENTS_URL}/${season}/${eventCode}/teams`)).json();
+	teams[season] = {};
+	teams[season][eventCode] = {};
+	fTeams.forEach(team => teams[season][eventCode][team.teamNumber] = {
+		teamNumber: team.teamNumber,
+		stats: team.stats
+	});
 }
 
 export async function getTeamData(season, eventCode) {
-    if (!storageContains(season, eventCode)) await populateStorage(season, eventCode);
-    return teams[season][eventCode];
+	if (!storageContains(season, eventCode)) await populateStorage(season, eventCode);
+	return teams[season][eventCode];
 }
 
 export async function getTeams(season, eventCode) {
-    if (!storageContains(season, eventCode)) await populateStorage(season, eventCode);
-    return Object.keys(teams[season][eventCode]);
+	if (!storageContains(season, eventCode)) await populateStorage(season, eventCode);
+	return Object.keys(teams[season][eventCode]);
+}
+
+export async function getEvents(season) {
+	const query = JSON.stringify({
+		query: `{
+          eventsSearch(season: 2023) {
+                name
+                code
+            }
+        }`
+	})
 }
